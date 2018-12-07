42a34eec1c472ccd87dce94f96662863e5e80b02
Jason-Baker-Safelite <45693148+Jason-Baker-Safelite@users.noreply.github.com>
2018-12-07 12:11:44 -0500

    Origin

---
+++ Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace NFL
+{
+    class Program
+    {
+        private static Excel.Workbook MyBook = null;
+        private static Excel.Application MyApp = null;
+        private static Excel.Worksheet MySheet = null;
+        private static Excel.Range MyRange = null;
+        public enum PlayerPosition
+        {
+            QB,
+            RB
+        };
+        public static void Main(string[] args)
+        {
+            string startTime = DateTime.Now.ToString();
+            Console.WriteLine("Start time: {0}", startTime);
+            ProcessSpreadsheet(SetUpExcel());
+            string endTime = DateTime.Now.ToString();
+            Console.WriteLine("End time: {0}", endTime);
+            Console.ReadKey();
+            //var go =
+            //from ord in (
+            //from playern in players
+            //where playern.Team == "QB"
+            //group playern by playern.Name into playerGroup
+            //select new Player1
+            //{
+            //    Name = playerGroup.Key,
+            //    Score = playerGroup.Sum(x => x.Score),
+            //    Player = playerGroup.
+            //}
+            //)
+            //orderby ord.Score descending
+            //select ord;
+            //var first = go.First();
+
+            Cleanup();
+        }
+        public static void ProcessSpreadsheet(object[,] ExcelArray)
+        {
+            Dictionary<string, Season> QBDictionary = new Dictionary<string, Season>();
+            Dictionary<string, Season> RBDictionary = new Dictionary<string, Season>();
+
+            for (int currentRow = 2; currentRow <= ExcelArray.GetLength(0); currentRow++)
+            {
+                string position = ExcelArray[currentRow, 22].ToString();
+                Validate(position, currentRow);
+                switch (position)
+                {
+                    case nameof(PlayerPosition.QB):
+                        ProcessPosition(QBDictionary, position, currentRow, ExcelArray);
+                        break;
+                    case nameof(PlayerPosition.RB):
+                        ProcessPosition(RBDictionary, position, currentRow, ExcelArray);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            string bestQB = QBDictionary.OrderByDescending(s => s.Value.PassingYards).First().Key;
+            double bestPassingYards = QBDictionary[bestQB].PassingYards;
+            Console.WriteLine("The best QB is {0} with {1:0,0} passing yards\n", bestQB, bestPassingYards);
+            string bestRB = RBDictionary.OrderByDescending(s => s.Value.RushingYards).First().Key;
+            double bestRushingYards = RBDictionary[bestRB].RushingYards;
+            Console.WriteLine("The best RB is {0} with {1:0,0} rushing yards", bestRB, bestRushingYards);
+        }
+        public static void ProcessPosition(Dictionary<string, Season> positionDictionary, string position, int currentRow, object[,] ExcelArray)
+        {
+            string player = ExcelArray[currentRow, 2].ToString();
+            Validate(player, currentRow);
+            double passingYards = (double)ExcelArray[currentRow, 11];
+            Validate(passingYards, currentRow);
+            double rushingYards = (double)ExcelArray[currentRow, 15];
+            Validate(rushingYards, currentRow);
+            if (positionDictionary.Count == 0)
+            {
+                Season season = AddSeason(currentRow, position, ExcelArray);
+                positionDictionary.Add(season.FullName, season);
+            }
+            else
+            {
+                if (positionDictionary.ContainsKey(player))
+                {
+                    if (position == "QB")
+                    {
+                        positionDictionary[player].PassingYards += passingYards;
+                    }
+                    else
+                    {
+                        positionDictionary[player].RushingYards += rushingYards;
+                    }
+                }
+                else
+                {
+                    Season season = AddSeason(currentRow, position, ExcelArray);
+                    positionDictionary.Add(season.FullName, season);
+                }
+            }
+        }
+        public static object[,] SetUpExcel()
+        {
+            MyApp = new Excel.Application
+            {
+                Visible = false
+            };
+            string XLS_PATH = "C:\\Users\\Jason.Baker\\Projects\\NFLAppConsole\\NFLAppConsole\\NFL_Small_Set.xlsx";
+            MyBook = MyApp.Workbooks.Open(XLS_PATH);
+            MySheet = (Excel.Worksheet)MyBook.Sheets["1999-2013 data"];
+            MyRange = MySheet.UsedRange;
+            object[,] objectArray = (object[,])MyRange.Value2;
+            return objectArray;
+        }
+        public static void Validate(string cellString, int cellRow)
+        {
+            if (String.IsNullOrEmpty(cellString))
+            {
+                string exceptionString = string.Format("invalid cell string in row {0}", cellRow.ToString());
+                throw new ArgumentException(exceptionString);
+            }
+        }
+        public static void Validate(double cellDouble, int cellRow)
+        {
+            if (double.IsNaN(cellDouble))
+            {
+                string exceptionString = string.Format("invalid yards value in row {0}", cellRow.ToString());
+                throw new ArgumentException(exceptionString);
+            }
+        }
+        private static Season AddSeason(int currentRow, string position, object[,] ExcelArray)
+        {
+            Season season = new Season
+            {
+                FullName = ExcelArray[currentRow, 2].ToString(),
+                PassingYards = (double)ExcelArray[currentRow, 11],
+                Position = position,
+                RushingYards = (double)ExcelArray[currentRow, 15]
+            };
+            return season;
+        }
+        private static void Cleanup()
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            Marshal.ReleaseComObject(MyRange);
+            Marshal.ReleaseComObject(MySheet);
+            MyBook.Close();
+            Marshal.ReleaseComObject(MyBook);
+            MyApp.Quit();
+            Marshal.ReleaseComObject(MyApp);
+        }
+    }
+}
---
+++ Season.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NFL
+{
+    public class Season
+    {
+        public string Position { get; set; }
+        public string FullName { get; set; }
+        public double PassingYards { get; set; }
+        public double RushingYards { get; set; }
+    }
+}
