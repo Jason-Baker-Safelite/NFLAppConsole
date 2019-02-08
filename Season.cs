4f734ad1811922d537555794aeef488122ade28d
terrymac49 <45694707+terrymac49@users.noreply.github.com>
2019-02-08 11:04:54 -0500

    Added FullName

--- Season.cs
+++ Season.cs
@@ -10,5 +10,8 @@ namespace NFL
         public string FullName { get; set; }
         public double PassingYards { get; set; }
         public double RushingYards { get; set; }
+        public string College{ get; set; }
+        public string Team{ get; set; }
+        public double Year{ get; set; }
     }
 }
