bbc60b1b5f0d1e0fd8213a1a19145eb7c60dc73a
Jason-Baker-Safelite <45693148+Jason-Baker-Safelite@users.noreply.github.com>
2019-02-11 10:27:17 -0500

    Big rewrite!  Now using a List<T> rather than Dictionary, and removed obsolete code/comments.

--- Program.cs
+++ Program.cs
@@ -1,24 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
+using System.Configuration;
 using System.Linq;
 using System.Runtime.InteropServices;
 using Excel = Microsoft.Office.Interop.Excel;
-using System.Configuration;
-using System.Collections.Specialized;
-
-//Added the config file
-//This file should be set to your locations of the excel and the sheet used
-//This file should be set to ignore on the .gitignore after your settings are setup
-//Be aware that some changes could be checked it that could change this but currently not anticipated
-// Randy comment
-//liping test
-// Jason comment 10:55 AM
-//checkout sourcetree
-// Jason new and improved comment 1/23/2019 10:13 AM
-// Jason comment 1/23/2019 10:18 AM
-// Jason comment 11:46 AM
-// another new comment 2/8/2019
 
 namespace NFL
 {
@@ -33,6 +18,16 @@ namespace NFL
             QB,
             RB
         };
+        public enum SpreadsheetColumn
+        {
+            yearColumn = 1,
+            playerColumn = 2,
+            teamColumn = 6,
+            passingYardsColumn = 11,
+            rushingYardsColumn = 15,
+            positionColumn = 22,
+            collegeColumn = 26
+        };
         public static void Main(string[] args)
         {
             PrintMenu();
@@ -42,12 +37,9 @@ namespace NFL
             //string endTime = DateTime.Now.ToString();
             //Console.WriteLine("End time: {0}", endTime);
             Console.ReadKey();
-
-
-            //Cleanup();
+            Cleanup();
         }
-
-        private static void PrintMenu()
+        public static void PrintMenu()
         {
             Console.WriteLine("Select Menu Item");
             Console.WriteLine("1. Search by Player");
@@ -55,63 +47,49 @@ namespace NFL
             Console.WriteLine("3. Search by Position");
             Console.WriteLine("4. Search by Year");
             Console.WriteLine("5. Search by College");
-            string Input = Console.ReadLine();
-            if (Input == "1")
+            string menuSelection = Console.ReadLine();
+            if (menuSelection == "1")
             {
                 Console.WriteLine("You selected 1");
-                SearchByPlayer();
+                SearchByPlayer(menuSelection);
             }
-            else if (Input == "2")
+            else if (menuSelection == "2")
             {
                 Console.WriteLine("You selected 2");
             }
-            else if (Input == "3")
+            else if (menuSelection == "3")
             {
                 Console.WriteLine("You selected 3");
             }
-            else if (Input == "4")
+            else if (menuSelection == "4")
             {
                 Console.WriteLine("You selected 4");
             }
-            else if (Input == "5")
+            else if (menuSelection == "5")
             {
                 Console.WriteLine("You selected Search by College");
-                SearchByCollege();
+                SearchByCollege(menuSelection);
             }
             else
             {
                 Console.WriteLine("Invalid Menu Selection");
                 PrintMenu();
             }
-
         }
-
-        private static void SearchByPlayer()
+        public static void SearchByPlayer(string menuNumber)
         {
             Console.WriteLine("Please Enter Player Name");
-            string input = Console.ReadLine();
-            Console.WriteLine("Search for " + input);
-            object[,] PlayerArray = SetUpExcel();
-            Dictionary<string, Season> PlayerCollection = LoadCollection(PlayerArray);
-            Dictionary<string, Season> NameCollection = PlayerCollection.Where(S => S.Value.FullName == input).ToDictionary(k => k.Key, k => k.Value);
+            string userInput = Console.ReadLine();
+            Console.WriteLine("Search for " + userInput);
+            object[,] playerArray = SetUpExcel();
+            List<Season> playerCollection = LoadCollection(playerArray);
+            List<Season> nameCollection = playerCollection.Where(s => s.FullName == userInput).ToList();
         }
-
-        private static Dictionary<string, Season> LoadCollection(object[,] ExcelArray)
-        {
-            Dictionary<string, Season> PlayerDictionary = new Dictionary<string, Season>();
-            for (int currentRow = 2; currentRow <= ExcelArray.GetLength(0); currentRow++)
-            {
-                Season season = AddSeason(currentRow, ExcelArray);
-                PlayerDictionary.Add(season.FullName + season.Team + season.Year, season);
-            }
-            return PlayerDictionary;
-        }
-
         //Dennis college search - start
-        private static void SearchByCollege()
+        public static void SearchByCollege(string input)
         {
             Console.WriteLine("Please Enter College Name");
-            string input = Console.ReadLine();
+            input = Console.ReadLine();
 
             if (String.IsNullOrEmpty(input))
             {
@@ -120,11 +98,11 @@ namespace NFL
             }
             else
             {
-                // Extract the rows matching the college (name / pos / college)
+                // Extract the rows matching the college (name / position / college)
                 object[,] PlayerArray = SetUpExcel();
-                Dictionary<string, Season> CollegeDict = LoadCollection(PlayerArray);
-                Dictionary<string, Season> NameCollection = CollegeDict.Where(S => S.Value.College == input).ToDictionary(k => k.Key, k => k.Value);
-                int count1 = CollegeDict.Count;
+                List<Season> collegeDict = LoadCollection(PlayerArray);
+                List<Season> nameCollection = collegeDict.Where(s => s.College == input).ToList();
+                int count1 = collegeDict.Count;
                 if (count1 > 1)
                 {
                     Console.WriteLine("College Name not unique");
@@ -135,81 +113,21 @@ namespace NFL
                 }
                 else
                 {
-                    Console.WriteLine("College data displayjj");
+                    Console.WriteLine("College data display");
                 }
             }
             //Dennis college search - end
         }
-
-        public static void ProcessSpreadsheet(object[,] ExcelArray)
+        public static List<Season> LoadCollection(object[,] ExcelArray)
         {
-            Dictionary<string, Season> QBDictionary = new Dictionary<string, Season>();
-            Dictionary<string, Season> RBDictionary = new Dictionary<string, Season>();
-
+            List<Season> spreadsheetCollection = new List<Season>();
             for (int currentRow = 2; currentRow <= ExcelArray.GetLength(0); currentRow++)
             {
-                string position = ExcelArray[currentRow, 22].ToString();
-                Validate(position, currentRow);
-                switch (position)
-                {
-                    case nameof(PlayerPosition.QB):
-                        ProcessPosition(QBDictionary, position, currentRow, ExcelArray);
-                        break;
-                    case nameof(PlayerPosition.RB):
-                        ProcessPosition(RBDictionary, position, currentRow, ExcelArray);
-                        break;
-                    default:
-                        break;
-                }
+                object[,] correctedArray = Validate(currentRow, ExcelArray);
+                Season season = AddSeason(currentRow, correctedArray);
+                spreadsheetCollection.Add(season);
             }
-            string bestQB = QBDictionary.OrderByDescending(s => s.Value.PassingYards).First().Key;
-            double bestPassingYards = QBDictionary[bestQB].PassingYards;
-            Console.WriteLine("The best QB is {0} with {1:0,0} passing yards\n", bestQB, bestPassingYards);
-            string bestRB = RBDictionary.OrderByDescending(s => s.Value.RushingYards).First().Key;
-            double bestRushingYards = RBDictionary[bestRB].RushingYards;
-            Console.WriteLine("The best RB is {0} with {1:0,0} rushing yards", bestRB, bestRushingYards);
-        }
-        public static void ProcessPosition(Dictionary<string, Season> positionDictionary, string position, int currentRow, object[,] ExcelArray)
-        {
-            string year = ExcelArray[currentRow, 1].ToString();
-            Validate(year, currentRow);
-            string player = ExcelArray[currentRow, 2].ToString();
-            Validate(player, currentRow);
-            string team = ExcelArray[currentRow, 6].ToString();
-            Validate(team, currentRow);
-            double passingYards = (double)ExcelArray[currentRow, 11];
-            Validate(passingYards, currentRow);
-            double rushingYards = (double)ExcelArray[currentRow, 15];
-            Validate(rushingYards, currentRow);
-            string postion = ExcelArray[currentRow, 22].ToString();
-            Validate(position, currentRow);
-            string college = ExcelArray[currentRow, 26].ToString();
-            Validate(college, currentRow);
-
-            if (positionDictionary.Count == 0)
-            {
-                Season season = AddSeason(currentRow, ExcelArray);
-                positionDictionary.Add(season.FullName, season);
-            }
-            else
-            {
-                if (positionDictionary.ContainsKey(player))
-                {
-                    if (position == "QB")
-                    {
-                        positionDictionary[player].PassingYards += passingYards;
-                    }
-                    else
-                    {
-                        positionDictionary[player].RushingYards += rushingYards;
-                    }
-                }
-                else
-                {
-                    Season season = AddSeason(currentRow, ExcelArray);
-                    positionDictionary.Add(season.FullName, season);
-                }
-            }
+            return spreadsheetCollection;
         }
         public static object[,] SetUpExcel()
         {
@@ -217,7 +135,6 @@ namespace NFL
             {
                 Visible = false
             };
-
             string XLS_PATH = ConfigurationManager.AppSettings.Get("ExcelPath");
             MyBook = MyApp.Workbooks.Open(XLS_PATH);
             MySheet = (Excel.Worksheet)MyBook.Sheets[ConfigurationManager.AppSettings.Get("SheetName")];
@@ -225,42 +142,49 @@ namespace NFL
             object[,] objectArray = (object[,])MyRange.Value2;
             return objectArray;
         }
-        public static void Validate(string cellString, int cellRow)
+        public static object[,] Validate(int spreadsheetRow, object[,] parmArray)
         {
-            if (String.IsNullOrEmpty(cellString))
+            if (double.IsNaN((double)parmArray[spreadsheetRow, (int)SpreadsheetColumn.yearColumn]))
             {
-                string exceptionString = string.Format("invalid cell string in row {0}", cellRow.ToString());
-                throw new ArgumentException(exceptionString);
+                parmArray[spreadsheetRow, (int)SpreadsheetColumn.yearColumn] = 0;
             }
-        }
-        public static void Validate(double cellDouble, int cellRow)
-        {
-            if (double.IsNaN(cellDouble))
+            if (String.IsNullOrEmpty(parmArray[spreadsheetRow, (int)SpreadsheetColumn.playerColumn].ToString()))
             {
-                string exceptionString = string.Format("invalid yards value in row {0}", cellRow.ToString());
-                throw new ArgumentException(exceptionString);
+                parmArray[spreadsheetRow, (int)SpreadsheetColumn.playerColumn] = " ";
             }
-        }
-        private static Season AddSeason(int currentRow, object[,] ExcelArray)
-        {
-            Season season = new Season
+            if (String.IsNullOrEmpty(parmArray[spreadsheetRow, (int)SpreadsheetColumn.teamColumn].ToString()))
             {
-                //                FullName = ExcelArray[currentRow, 2].ToString(),
-                //                PassingYards = (double)ExcelArray[currentRow, 11],
-                //                Position = ExcelArray[currentRow, 22].ToString(),
-                //                RushingYards = (double)ExcelArray[currentRow, 15]
-
-                //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-                //Dennis - NEED TO VALIDATE NUMBERIC FIELDS - ROW 7531 - Victor Cruz
-                //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-                Year = (double)ExcelArray[currentRow, 1],
-                FullName = ExcelArray[currentRow, 2].ToString(),
-                Team = ExcelArray[currentRow, 6].ToString(),
-                PassingYards = (double)ExcelArray[currentRow, 11],
-                RushingYards = (double)ExcelArray[currentRow, 15],
-                Position = ExcelArray[currentRow, 22].ToString(),
-                College = ExcelArray[currentRow, 26].ToString()
+                parmArray[spreadsheetRow, (int)SpreadsheetColumn.teamColumn] = " ";
+            }
+            if (String.IsNullOrWhiteSpace(Convert.ToString(parmArray[spreadsheetRow, (int)SpreadsheetColumn.passingYardsColumn])))
+            {
+                parmArray[spreadsheetRow, (int)SpreadsheetColumn.passingYardsColumn] = 0;
+            }
+            if (String.IsNullOrWhiteSpace(Convert.ToString(parmArray[spreadsheetRow, (int)SpreadsheetColumn.rushingYardsColumn])))
+            {
+                parmArray[spreadsheetRow, (int)SpreadsheetColumn.rushingYardsColumn] = 0;
+            }
+            if (String.IsNullOrEmpty(parmArray[spreadsheetRow, (int)SpreadsheetColumn.positionColumn].ToString()))
+            {
+                parmArray[spreadsheetRow, (int)SpreadsheetColumn.positionColumn] = " ";
+            }
+            if (String.IsNullOrEmpty(parmArray[spreadsheetRow, (int)SpreadsheetColumn.collegeColumn].ToString()))
+            {
+                parmArray[spreadsheetRow, (int)SpreadsheetColumn.collegeColumn] = " ";
+            }
+            return parmArray;
+        }
+        public static Season AddSeason(int currentRow, object[,] ExcelArray)
+        {
+            Season season = new Season()
+            {
+                Year = Convert.ToDouble(ExcelArray[currentRow, (int)SpreadsheetColumn.yearColumn]),
+                FullName = ExcelArray[currentRow, (int)SpreadsheetColumn.playerColumn].ToString(),
+                Team = ExcelArray[currentRow, (int)SpreadsheetColumn.teamColumn].ToString(),
+                PassingYards = Convert.ToDouble(ExcelArray[currentRow, (int)SpreadsheetColumn.passingYardsColumn]),
+                RushingYards = Convert.ToDouble(ExcelArray[currentRow, (int)SpreadsheetColumn.rushingYardsColumn]),
+                Position = ExcelArray[currentRow, (int)SpreadsheetColumn.positionColumn].ToString(),
+                College = ExcelArray[currentRow, (int)SpreadsheetColumn.collegeColumn].ToString()
             };
             return season;
         }
